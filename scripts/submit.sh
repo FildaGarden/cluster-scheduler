b10ba05308719c05c240a03b9283595e000540c8
Filip Zahradnik <filipzahradnik22@gmail.com>
2026-04-02 00:49:40 +0200

    Added script for simulating job request

---
+++ scripts/submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Adresa tvého Mastera (změň, pokud se změní IP)
+MASTER="http://192.168.0.111:8080"
+
+# Statické parametry pro rychlé testování
+COMMAND="echo 'Testovaci uloha spoustena pres script'; sleep 5; echo 'Uloha hotova'"
+CPU=1
+MEM=128
+
+echo "🚀 Odesílám testovací úlohu na Mastera ($MASTER)..."
+
+curl -X POST "$MASTER/submit" -d "{
+  \"Command\": \"$COMMAND\",
+  \"CPUCores\": $CPU,
+  \"MemoryMB\": $MEM
+}"
+
+echo -e "\n✅ Odesláno."
